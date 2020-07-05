009d886025e6aba145ce2e949728d4ea4961b858
yokawasa <yokawasa@gmail.com>
2020-07-05 12:45:36 +0900

    initial entry

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log
+
+All notable changes to the "action-setup-acctl" will be documented in this file.
+
+## v0.1.0
+- Initial release
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,178 @@
+import * as os from 'os'
+import * as path from 'path'
+import * as util from 'util'
+import * as fs from 'fs'
+
+import * as toolCache from '@actions/tool-cache'
 import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+const defaultKubectlVersion = '1.18.2'
+const defaultKustomizeVersion = '3.5.5'
+const defaultHelmVersion = '2.16.7'
+const defaultHelmv3Version = '3.2.1'
+const defaultKubevalVersion = '0.15.0'
+const defaultConftestVersion = '0.19.0'
+const defaultYqVersion = 'latest'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+interface Tool {
+  name: string
+  defaultVersion: string
+  isArchived: boolean
+  commandPathInPackage: string
+}
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+const Tools: Tool[] = [
+  {
+    name: 'kubectl',
+    defaultVersion: defaultKubectlVersion,
+    isArchived: false,
+    commandPathInPackage: 'kubectl'
+  },
+  {
+    name: 'kustomize',
+    defaultVersion: defaultKustomizeVersion,
+    isArchived: true,
+    commandPathInPackage: 'kustomize'
+  },
+  {
+    name: 'helm',
+    defaultVersion: defaultHelmVersion,
+    isArchived: true,
+    commandPathInPackage: 'linux-amd64/helm'
+  },
+  {
+    name: 'helmv3',
+    defaultVersion: defaultHelmv3Version,
+    isArchived: true,
+    commandPathInPackage: 'linux-amd64/helm'
+  },
+  {
+    name: 'kubeval',
+    defaultVersion: defaultKubevalVersion,
+    isArchived: true,
+    commandPathInPackage: 'kubeval'
+  },
+  {
+    name: 'conftest',
+    defaultVersion: defaultConftestVersion,
+    isArchived: true,
+    commandPathInPackage: 'conftest'
+  },
+  {
+    name: 'yq',
+    defaultVersion: defaultYqVersion,
+    isArchived: false,
+    commandPathInPackage: 'yq_linux_amd64'
+  }
+]
+
+function getDownloadURL(commandName: string, version: string): string {
+  switch (commandName) {
+    case 'kubectl':
+      return util.format(
+        'https://storage.googleapis.com/kubernetes-release/release/v%s/bin/linux/amd64/kubectl',
+        version
+      )
+    case 'kustomize':
+      return util.format(
+        'https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv%s/kustomize_v%s_linux_amd64.tar.gz ',
+        version,
+        version
+      )
+    case 'helm':
+      return util.format(
+        'https://get.helm.sh/helm-v%s-linux-amd64.tar.gz ',
+        version
+      )
+    case 'helm3':
+      return util.format(
+        'https://get.helm.sh/helm-v%s-linux-amd64.tar.gz ',
+        version
+      )
+    case 'kubeval':
+      return util.format(
+        'https://github.com/instrumenta/kubeval/releases/download/%s/kubeval-linux-amd64.tar.gz',
+        version
+      )
+    case 'conftest':
+      return util.format(
+        'https://github.com/open-policy-agent/conftest/releases/download/v%s/conftest_%s_Linux_x86_64.tar.gz',
+        version,
+        version
+      )
+    case 'yq':
+      return util.format(
+        'https://github.com/mikefarah/yq/releases/%s/download/yq_linux_amd64',
+        version
+      )
+    default:
+      return ''
   }
 }
 
-run()
+async function downloadTool(version: string, tool: Tool): Promise<string> {
+  let cachedToolpath = toolCache.find(tool.name, version)
+  let commandPath = ''
+
+  if (!cachedToolpath) {
+    const downloadURL = getDownloadURL(tool.name, version)
+
+    try {
+      const packagePath = await toolCache.downloadTool(downloadURL)
+
+      if (tool.isArchived) {
+        fs.mkdirSync(`${packagePath}_extracted`)
+
+        const extractedDir = await toolCache.extractTar(
+          packagePath,
+          `${packagePath}_extracted`
+        )
+        commandPath = util.format(
+          '%s/%s',
+          extractedDir,
+          tool.commandPathInPackage
+        )
+      } else {
+        commandPath = packagePath
+      }
+    } catch (exception) {
+      throw new Error(`Download ${tool.name} Failed! (url: ${downloadURL})`)
+    }
+    cachedToolpath = await toolCache.cacheFile(
+      commandPath,
+      tool.name,
+      tool.name,
+      version
+    )
+  }
+
+  const ecctlPath = path.join(cachedToolpath, tool.name)
+  fs.chmodSync(ecctlPath, '777')
+  return ecctlPath
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+async function run() {
+  if (os.type().match(/^Win/)) {
+    throw new Error('Windows is not supported OS!')
+  }
+  // eslint-disable-next-line no-console
+  console.log(`os type: '${os.type()}'`)
+
+  // eslint-disable-next-line github/array-foreach
+  Tools.forEach(async function(tool) {
+    let toolVersion = core.getInput(tool.name, {required: false})
+    if (!toolVersion) {
+      toolVersion = tool.defaultVersion
+    }
+    const cachedPath = await downloadTool(toolVersion, tool)
+    core.addPath(path.dirname(cachedPath))
+    // eslint-disable-next-line no-console
+    console.log(
+      `${tool.name} version '${toolVersion}' has been cached at ${cachedPath}`
+    )
+    core.setOutput(`${tool.name}-path`, cachedPath)
+  })
+}
+
+run().catch(core.setFailed)
