4d0df84edd1bc3b71760e5dd7740b5b128b68c51
Yoichi Kawasaki <yokawasa@gmail.com>
2023-02-20 06:44:33 +0900

    upgrade default tools version
    Signed-off-by: Yoichi Kawasaki <yokawasa@gmail.com>

--- dist/index.js
+++ dist/index.js
@@ -6,20 +6,37 @@
 
 "use strict";
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const os = __importStar(__nccwpck_require__(2037));
@@ -28,18 +45,18 @@ const util = __importStar(__nccwpck_require__(3837));
 const fs = __importStar(__nccwpck_require__(7147));
 const toolCache = __importStar(__nccwpck_require__(7784));
 const core = __importStar(__nccwpck_require__(2186));
-const defaultKubectlVersion = '1.20.2';
-const defaultKustomizeVersion = '4.5.7';
-const defaultHelmVersion = '3.6.3';
+const defaultKubectlVersion = '1.24.10';
+const defaultKustomizeVersion = '5.0.0';
+const defaultHelmVersion = '3.11.1';
 const defaultHelmv2Version = '2.17.0';
 const defaultKubevalVersion = '0.16.1';
 const defaultKubeconformVersion = '0.5.0';
-const defaultConftestVersion = '0.19.0';
-const defaultYqVersion = '4.7.1';
-const defaultRancherVersion = '2.4.10';
-const defaultTiltVersion = '0.18.11';
-const defaultSkaffoldVersion = '1.20.0';
-const defaultKubeScoreVersion = '1.10.1';
+const defaultConftestVersion = '0.39.0';
+const defaultYqVersion = '4.30.7';
+const defaultRancherVersion = '2.7.0';
+const defaultTiltVersion = '0.31.2';
+const defaultSkaffoldVersion = '2.1.0';
+const defaultKubeScoreVersion = '1.16.1';
 const Tools = [
     {
         name: 'kubectl',
